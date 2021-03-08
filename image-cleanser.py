2ccc6948e88a32c1aa65722684868a9ca622514f
yottalogical <email@yottalogical.com>
2021-03-07 22:05:23 -0500

    Add more verbose command-line arguments

--- image-cleanser.py
+++ image-cleanser.py
@@ -2,21 +2,49 @@
 
 import numpy as np
 from PIL import Image
-from sys import argv
+from sys import argv, stderr
+from getopt import gnu_getopt, GetoptError
 
 
-def median(arrays):
+def median_image(images):
+	arrays = [np.asarray(image) for image in images]
+	
 	dtype = arrays[0].dtype
 	for array in arrays:
 		assert array.dtype == dtype
 	
-	return np.median(arrays, axis=0).astype(dtype)
+	return Image.fromarray(np.median(arrays, axis=0).astype(dtype))
+
+
+def process_argv():
+	try:
+		options, arguments = gnu_getopt(argv[1:], 'o:h', ['output =', 'help'])
+		options = dict(options)
+		
+		if ('-h' in options) or ('--help' in options) or (len(arguments) == 0):
+			print('Usage: ' + argv[0] + ' INPUT_FILES [-o, --output] OUTPUT_FILE')
+			exit()
+		else:
+			return arguments, options.get('-o', options.get('--options'))
+	except GetoptError as err:
+		print(err, file=stderr)
+		exit(1)
 
 
 def main():
-	input_image_arrays = [np.asarray(Image.open(str(i) + '.jpeg')) for i in range(int(argv[1]))]
+	input_filenames, output_filename = process_argv()
 	
-	Image.fromarray(median(input_image_arrays)).save('output.jpeg')
+	try:
+		input_images = [Image.open(filename) for filename in input_filenames]
+		output_image = median_image(input_images)
+		
+		if output_filename is not None:
+			output_image.save(output_filename)
+		else:
+			output_image.show()
+	except OSError as err:
+		print(err, file=stderr)
+		exit(1)
 
 
 if __name__ == '__main__':
